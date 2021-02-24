85547f39460e5733c4880d96f5e76601bcec7568
bvictori0927 <78389518+bvictori0927@users.noreply.github.com>
2021-02-23 20:29:03 -0500

    Update cipher.py

--- .github/workflows/cipher.py
+++ .github/workflows/cipher.py
@@ -1,53 +1,50 @@
-''' 
-Caesar Cipher program
-CaesarCipher.py
-BMeadows
-Used to encry/decrypt data
-'''
 
-#Bringing in the String library to generate character base 
-import string
+import string    # Import the string library to generate our character list
 
 
-#Prompt the user 
-message= input("Enter the message or phrase to be encrypted or decrypted: ")
-#Get the key from the user 
+# Prompt the user for a plain text string to be encrypted.
+message = input("Enter the message to be encrypted here: ")
+
+
+# Prompt the user for a key size. Verify that is is a int type.
 while True:
     try:
-        key = int(input('Please enter your key size: '))
+        key = int(input("Please enter your key size: "))
         break
-    except ValueError as error: 
+    except ValueError as error:
         print(error)
-        print('Please enter an integer for the key and try again.')
+        print("Please enter an integer for the key and try again.")
+
+# Set the mode to either encrypt or decrypt
+mode = input("Please enter E for encrypt and D for decrypt: ")
+
+# Create Character Base 
+SYMBOLS = string.ascii_letters + string.digits + string.punctuation + " "
+
+# To store our translated message
+translated = ''
+
+for symbol in message:    
+    if symbol in SYMBOLS: 
+        symbolIndex = SYMBOLS.find(symbol)  
+
+        # The shift
+        if mode in ['encrypt', 'ENCRYPT', 'e', 'E']:
+            translatedIndex = symbolIndex + key
+        elif mode in ['decrypt', 'DECRYPT', 'd', 'D']:
+            translatedIndex = symbolIndex - key
+
+            # What do I do when it wraps around? There's a script for that!
+        if translatedIndex >= len(SYMBOLS):
+            translatedIndex -= len(SYMBOLS)
+        elif translatedIndex < 0:
+            translatedIndex += len(SYMBOLS)
+
         
-#getting the mode setting
-mode=input('Please enter E for encrypt and D for decrypt!')
-
-
-#Create my character base 
-SYMBOLS= string.ascii_letters + string.punctuation + string.digits + " "
-#to store translated message
-
-translated= ''
-#Code to encrypt/decrypt
-for symbol in message:
-    if symbol in SYMBOLS:
-        symbolIndex = SYMBOLS.find(symbol)
-
-## The shift 
-    if mode in['e','E','ENCRYPT','encrypt']:
-        translatedIndex = symbolIndex + key
-    elif mode in ['DECRYPT','decrypt','D','d']:
-        translatedIndex = symbolIndex - key
-       
-#What do I do when it wraps around? There's a script for that!
-    if translatedIndex >= len(SYMBOLS):
-        translatedIndex -= len(SYMBOLS)
-    elif translatedIndex <= 0:
-        translatedIndex += [len(SYMBOLS)]
-
         translated += SYMBOLS[translatedIndex]
     else:
+        
         translated += symbol
-print(translated)
 
+
+print(translated)
